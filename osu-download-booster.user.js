586b89dd8d6d5f23b0a83c223ddf124a3c7fdb71
Azure99 <961523404@qq.com>
2020-06-09 00:16:30 +0800

    Update sayobot api

--- osu-download-booster.user.js
+++ osu-download-booster.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Osu Download Booster
 // @namespace    https://www.rainng.com/
-// @version      1.7
+// @version      1.8
 // @description  Osu谱面下载加速, 为中国玩家打造, 支持Rainng(Azure99维护)和Sayo(小夜维护)的镜像
 // @author       Azure99
 // @homepage     https://www.rainng.com/osu-download-booster
@@ -19,7 +19,7 @@
 
 (function () {
     'use strict';
-    const SAYO_URL = 'https://osu.sayobot.cn/osu.php?s=';
+    const SAYO_URL = 'https://txy1.sayobot.cn/beatmaps/download/full/';
 
     let lastId = -1;
     let latestDownloadUrl = '';
