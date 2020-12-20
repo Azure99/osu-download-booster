42c1b57877763ca2799c6a8b0c4041eaf036e942
Azure99 <961523404@qq.com>
2020-12-20 21:54:27 +0800

    Update osu-download-booster.user.js

--- osu-download-booster.user.js
+++ osu-download-booster.user.js
@@ -1,11 +1,11 @@
 // ==UserScript==
 // @name         Osu Download Booster
 // @namespace    https://www.rainng.com/
-// @version      1.8
+// @version      1.9
 // @description  Osu谱面下载加速, 为中国玩家打造, 支持Rainng(Azure99维护)和Sayo(小夜维护)的镜像
 // @author       Azure99
 // @homepage     https://www.rainng.com/osu-download-booster
-// @supportURL   https://github.com/Azure99/osu-download-booster/issues
+// @supportURL   https://www.rainng.com/osu-download-booster
 // @icon         https://osu.ppy.sh/favicon.ico
 // @include      http*://osu.ppy.sh/*
 // @grant        GM_xmlhttpRequest
