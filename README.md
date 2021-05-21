1adb1d6adca41df9dfa5b61d8d80fd9adaf79261
SpaceSkyNet <spaceskynet@126.com>
2021-05-21 12:14:35 +0800

    add chimu image

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Osu Download Booster
 Provide a near-original, fast and stable way to download osu! beatmap<br/>
-Mirror By **Rainng & Sayobot**
+Mirror By **Rainng & Sayobot & chimu**
 
 # How to use
 [Greasy Fork](https://greasyfork.org/zh-CN/scripts/395607-osu-download-booster)
--- osu-download-booster.user.js
+++ osu-download-booster.user.js
@@ -1,9 +1,9 @@
 // ==UserScript==
 // @name         Osu Download Booster
 // @namespace    https://www.rainng.com/
-// @version      2.1
-// @description  Osu谱面下载加速, 为中国玩家打造, 支持Rainng(Azure99维护)和Sayo(小夜维护)的镜像
-// @author       Azure99
+// @version      2.2
+// @description  Osu谱面下载加速, 为中国玩家打造, 支持Rainng(Azure99维护)、Sayo(小夜维护)以及chimu的镜像
+// @author       Azure99 & SpaceSkyNet
 // @homepage     https://www.rainng.com/osu-download-booster
 // @supportURL   https://www.rainng.com/osu-download-booster
 // @icon         https://osu.ppy.sh/favicon.ico
@@ -14,6 +14,8 @@
 // @connect      bm6.ppy.sh
 // @connect      bm7.ppy.sh
 // @connect      bm8.ppy.sh
+// @connect      api.chimu.moe
+// @connect      ipfs.chimu.moe
 // @license      MIT License
 // ==/UserScript==
 
@@ -22,8 +24,12 @@
     const SAYO_URL = 'https://txy1.sayobot.cn/beatmaps/download/full/';
 
     let lastId = -1;
-    let latestDownloadUrl = '';
-    let running = false;
+    let Azure99_latestDownloadUrl = '';
+    let chimu_latestDownloadUrl = '';
+    let Azure99_running = false;
+    let chimu_running = false;
+    let Azure99_reponseString = '';
+    let chimu_reponseString = '';
 
     setBeatmapTimer();
     setButtonTimer();
@@ -61,17 +67,18 @@
         if (lastId !== currentId) {
             lastId = currentId;
             if (currentId > 0) {
-                getDownloadUrl(currentId);
+                getAzure99DownloadUrl(currentId);
+                getChimuDownloadUrl(currentId);
             }
         }
     }
 
     function insertButton() {
-        let needInsert = $('.js-beatmapset-download-link').length >= 1 && $('.btn-osu-mirror').length === 0;
+        let needInsert = $('.js-beatmapset-download-link').length >= 0 && $('.btn-osu-mirror-Azure99').length === 0;
         if (needInsert) {
             $('.beatmapset-header__buttons').append(
-                '<a href="' + latestDownloadUrl + '" data-turbolinks="false"\n' +
-                '   class="btn-osu-mirror btn-osu-big btn-osu-big--beatmapset-header js-beatmapset-download-link"><span\n' +
+                '<a href="' + Azure99_latestDownloadUrl + '" data-turbolinks="false"\n' +
+                '   class="btn-osu-mirror-Azure99 btn-osu-big btn-osu-big--beatmapset-header js-beatmapset-download-link"><span\n' +
                 '        class="btn-osu-big__content "><span class="btn-osu-big__left"><span class="btn-osu-big__text-top">镜像下载</span><span\n' +
                 '        class="btn-osu-hint btn-osu-big__text-bottom">获取地址中...</span></span><span class="btn-osu-big__icon"><span class="fa-fw"><i\n' +
                 '        class="fas fa-download"></i></span></span></span></a>'
@@ -83,18 +90,26 @@
                 '        class="btn-osu-hint btn-osu-big__text-bottom">由Sayo加速</span></span><span class="btn-osu-big__icon"><span class="fa-fw"><i\n' +
                 '        class="fas fa-download"></i></span></span></span></a>'
             );
+            $('.beatmapset-header__buttons').append(
+                '<a href="' + chimu_latestDownloadUrl + '" data-turbolinks="false"\n' +
+                '   class="btn-osu-mirror-chimu btn-osu-big btn-osu-big--beatmapset-header js-beatmapset-download-link"><span\n' +
+                '        class="btn-osu-big__content "><span class="btn-osu-big__left"><span class="btn-osu-big__text-top">镜像下载</span><span\n' +
+                '        class="btn-osu-hint btn-osu-big__text-bottom">获取地址中...</span></span><span class="btn-osu-big__icon"><span class="fa-fw"><i\n' +
+                '        class="fas fa-download"></i></span></span></span></a>'
+            );
         }
     }
 
     function updateButton() {
-        let btn = $('.btn-osu-mirror')[0];
-        if (btn && btn.href !== latestDownloadUrl) {
-            btn.href = latestDownloadUrl;
-            if (btn.href.startsWith('https://bm')) {
-                $('.btn-osu-hint')[0].innerText = '由Azure99加速';
-            } else {
-                $('.btn-osu-hint')[0].innerText = '获取地址中...';
-            }
+        let Azure99_btn = $('.btn-osu-mirror-Azure99')[0];
+        if (Azure99_btn && Azure99_btn.href !== Azure99_latestDownloadUrl) {
+            Azure99_btn.href = Azure99_latestDownloadUrl;
+            $('.btn-osu-hint')[0].innerText = Azure99_reponseString;
+        }
+        let chimu_btn = $('.btn-osu-mirror-chimu')[0];
+        if (chimu_btn && chimu_btn.href !== chimu_latestDownloadUrl) {
+            chimu_btn.href = chimu_latestDownloadUrl;
+            $('.btn-osu-hint')[2].innerText = chimu_reponseString;
         }
     }
 
@@ -109,13 +124,14 @@
         return -1;
     }
 
-    function getDownloadUrl(mapId) {
-        if (running) {
+    function getAzure99DownloadUrl(mapId) {
+        if (Azure99_running) {
             return;
         }
-        running = true;
-        latestDownloadUrl = '';
-        let request = GM_xmlhttpRequest({
+        Azure99_running = true;
+        Azure99_latestDownloadUrl = '';
+        Azure99_reponseString = '获取地址中...';
+        let Azure99_request = GM_xmlhttpRequest({
             method: 'GET',
             url: '/beatmapsets/' + mapId + '/download',
             headers: {
@@ -123,10 +139,42 @@
             },
             onreadystatechange: function (status) {
                 let url = status.finalUrl.toString();
-                if (url.startsWith('https://bm') && status.readyState === 2) {
-                    running = false;
-                    request.abort();
-                    latestDownloadUrl = url.replace('.ppy.sh/', '.osu.rainng.com/');
+                if (url.startsWith('https://bm') && status.readyState >= 2) {
+                    Azure99_running = false;
+                    Azure99_request.abort();
+                    url = url.replace('.ppy.sh/', '.osu.rainng.com/');
+                    Azure99_latestDownloadUrl = url;
+                    console.log(url);
+                    Azure99_reponseString = '由Azure99加速';
+                }
+            }
+        });
+    }
+
+    function getChimuDownloadUrl(mapId) {
+        if (chimu_running) {
+            return;
+        }
+        chimu_running = true;
+        chimu_latestDownloadUrl = '';
+        chimu_reponseString = '获取地址中...';
+        let chimu_request = GM_xmlhttpRequest({
+            method: 'GET',
+            url: 'https://api.chimu.moe/v1/download/' + mapId + '?n=1',
+            onreadystatechange: function (status) {
+                let url = status.finalUrl.toString();
+                if (url.startsWith('https://ipfs') && status.readyState >= 2 && status.status === 200) {
+                    chimu_running = false;
+                    chimu_request.abort();
+                    chimu_latestDownloadUrl = url;
+                    console.log(url);
+                    chimu_reponseString = '由chimu加速';
+                }
+                else if(status.readyState >= 2 && status.status !== 200){
+                    chimu_running = false;
+                    chimu_request.abort();
+                    console.log('chimu镜像站暂无此铺面');
+                    chimu_reponseString = 'chimu暂无此铺面';
                 }
             }
         });
